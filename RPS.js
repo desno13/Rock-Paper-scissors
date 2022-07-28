0840d14c0c178ec4de1935a1993de077ecc9022a
Des Dowling <desdowling1999@gmail.com>
2022-07-28 15:12:50 +0100

    Formatting fixed

--- RPS.js
+++ RPS.js
@@ -5,18 +5,27 @@ function GetComputerChoice() {
     if (choice == 1){
         choice = 'rock';
         div.textContent = 'Computer chooses rock';
+        div.style.display = "flex";
+        div.style.alignContent = "center";
+        div.style.justifyContent = "center";
         console.log("Comp choice is r")
         return choice;
     }
     if(choice == 2){
         choice = 'paper';
         div.textContent = 'Computer chooses paper';
+        div.style.display = "flex";
+        div.style.alignContent = "center";
+        div.style.justifyContent = "center";
         console.log("Comp choice is p")
         return choice;
     }
     else {
         choice = 'scissors';
         div.textContent = 'Computer chooses scissors';
+        div.style.display = "flex";
+        div.style.alignContent = "center";
+        div.style.justifyContent = "center";
         console.log("Comp choice is s")
         return choice;
     }
@@ -48,30 +57,45 @@ function round(Comp, Player){
     console.log(Player)
     if (Comp == Player){
         win.textContent = 'Game tied';
+        win.style.display = "flex";
+        win.style.alignContent = "center";
+        win.style.justifyContent = "center";
         console.log("Game tied")
         carry(0);
         return;  
     }
     if (Player == 'rock'& Comp == 'scissors'){
         win.textContent = 'Winner Winner Chicken Dinner';
+        win.style.display = "flex";
+        win.style.alignContent = "center";
+        win.style.justifyContent = "center";
         console.log('Winner Winner Chicken Dinner')
         carry(1);
         return; 
     }
     if (Player == 'paper'& Comp == 'rock'){
         win.textContent = 'Winner Winner Chicken Dinner';
+        win.style.display = "flex";
+        win.style.alignContent = "center";
+        win.style.justifyContent = "center";
         console.log('Winner Winner Chicken Dinner')
         carry(1);
         return; 
     }
     if (Player == 'scissors'& Comp == 'paper'){
         win.textContent = 'Winner Winner Chicken Dinner';
+        win.style.display = "flex";
+        win.style.alignContent = "center";
+        win.style.justifyContent = "center";
         console.log('Winner Winner Chicken Dinner')
         carry(1);
         return;
     }
     else{
         win.textContent = 'You lose';
+        win.style.display = "flex";
+        win.style.alignContent = "center";
+        win.style.justifyContent = "center";
         console.log('You lose. LOLOLOLOLOLOL')
         carry(2);
         return;
@@ -82,21 +106,38 @@ function carry(roll){
 
     if(roll == 0 ){
         score.textContent = 'Computer = '+initialC +' Player = ' + (initialP);
+        score.style.display = "flex";
+        score.style.alignContent = "center";
+        score.style.justifyContent = "center";
     }
     if (roll == 1){
         initialP++
         score.textContent = 'Computer = '+initialC +' Player = ' + (initialP);
+        score.style.display = "flex";
+        score.style.alignContent = "center";
+        score.style.justifyContent = "center";
     }
     if (roll == 2){
         initialC++;
         score.textContent = 'Computer = '+initialC +' Player = ' + (initialP);
+        score.style.display = "flex";
+        score.style.alignContent = "center";
+        score.style.justifyContent = "center";
     }
 
 
 
 }
 
+const container = document.querySelector('#container')
+container.style.display= "flex";
+container.style.alignContent = "center";
+container.style.justifyContent = "center";
 
+const h = document.querySelector('h')
+h.style.display= "flex";
+h.style.alignContent = "center";
+h.style.justifyContent = "center";
 
 const result = document.querySelector('#result')
 const div = document.createElement("div");
